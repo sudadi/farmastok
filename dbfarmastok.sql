e321f0dfc93c62c72ea994ea712cb146ba661862
sudadi <sudadi.rso@gmail.com>
2017-05-10 16:09:12 +0700

    db terbaru, berisi beberapa sampel data. hanya utk pertama kali dan akan menghapus semua data yang ada jika sudah ada dbfarmastok sebelumnya.

--- dbfarmastok.sql
+++ dbfarmastok.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 04 Mei 2017 pada 14.04
+-- Generation Time: 10 Mei 2017 pada 10.54
 -- Versi Server: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -23,9 +23,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `tfifo`
+--
+
+DROP TABLE IF EXISTS `tfifo`;
+CREATE TABLE `tfifo` (
+  `id` int(11) NOT NULL,
+  `masuk` varchar(15) NOT NULL,
+  `keluar` varchar(15) NOT NULL,
+  `kdobat` varchar(15) NOT NULL,
+  `QTY` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tobat`
 --
 
+DROP TABLE IF EXISTS `tobat`;
 CREATE TABLE `tobat` (
   `id` int(11) NOT NULL,
   `kdobat` varchar(15) NOT NULL,
@@ -41,8 +57,8 @@ CREATE TABLE `tobat` (
 --
 
 INSERT INTO `tobat` (`id`, `kdobat`, `nmobat`, `satuan`, `limitstok`, `hbeli`, `hjual`) VALUES
-(1, '0123001', 'Parasetamot', 'tablet', 10, 100, 200),
-(9, '0123002', 'Konidin', 'kaplet', 1, 1, 1);
+(1, '0123001', 'Parasetamol', 'tablet', 10, 100, 200),
+(2, '0123002', 'Konidin', 'kaplet', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -50,6 +66,7 @@ INSERT INTO `tobat` (`id`, `kdobat`, `nmobat`, `satuan`, `limitstok`, `hbeli`, `
 -- Struktur dari tabel `tobatin`
 --
 
+DROP TABLE IF EXISTS `tobatin`;
 CREATE TABLE `tobatin` (
   `id` int(11) NOT NULL,
   `kdtrans` varchar(15) NOT NULL,
@@ -57,6 +74,7 @@ CREATE TABLE `tobatin` (
   `tgltrans` date NOT NULL,
   `kdobat` varchar(15) NOT NULL,
   `qty` int(11) NOT NULL,
+  `hbeli` int(11) NOT NULL,
   `keluar` int(6) NOT NULL,
   `sisa` int(6) NOT NULL,
   `stat` varchar(10) NOT NULL
@@ -66,10 +84,10 @@ CREATE TABLE `tobatin` (
 -- Dumping data untuk tabel `tobatin`
 --
 
-INSERT INTO `tobatin` (`id`, `kdtrans`, `kdsupp`, `tgltrans`, `kdobat`, `qty`, `keluar`, `sisa`, `stat`) VALUES
-(21, '20170502001', '020230', '2017-05-02', '0123001', 3, 2, 1, ''),
-(22, '20170502001', '020230', '2017-04-02', '0123002', 6, 4, 2, ''),
-(23, '20170502002', '020230', '2017-05-02', '0123002', 6, 0, 6, '');
+INSERT INTO `tobatin` (`id`, `kdtrans`, `kdsupp`, `tgltrans`, `kdobat`, `qty`, `hbeli`, `keluar`, `sisa`, `stat`) VALUES
+(1, '20170502001', '020230', '2017-05-02', '0123001', 3, 0, 0, 3, ''),
+(2, '20170502001', '020230', '2017-04-02', '0123002', 6, 0, 0, 6, ''),
+(3, '20170502002', '020230', '2017-05-02', '0123002', 6, 0, 0, 6, '');
 
 -- --------------------------------------------------------
 
@@ -77,51 +95,134 @@ INSERT INTO `tobatin` (`id`, `kdtrans`, `kdsupp`, `tgltrans`, `kdobat`, `qty`, `
 -- Struktur dari tabel `tobatout`
 --
 
+DROP TABLE IF EXISTS `tobatout`;
 CREATE TABLE `tobatout` (
   `id` int(11) NOT NULL,
   `kdtrans` varchar(15) NOT NULL,
   `kdsat` varchar(5) NOT NULL,
   `kdobat` varchar(15) NOT NULL,
   `qty` int(3) NOT NULL,
+  `hjual` int(11) NOT NULL,
   `tgltrans` date NOT NULL,
   `opt` varchar(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tobatout`
---
-
-INSERT INTO `tobatout` (`id`, `kdtrans`, `kdsat`, `kdobat`, `qty`, `tgltrans`, `opt`) VALUES
-(16, '20170504010', '0002', '0123002', 2, '2017-05-04', ''),
-(15, '20170504009', '0002', '0123002', 2, '2017-05-04', ''),
-(17, '20170504010', '0002', '0123001', 2, '2017-05-04', '');
-
---
 -- Trigger `tobatout`
 --
+DROP TRIGGER IF EXISTS `OBATOUT_AFDELETE`;
+DELIMITER $$
+CREATE TRIGGER `OBATOUT_AFDELETE` AFTER DELETE ON `tobatout` FOR EACH ROW BEGIN
+SET @KDOBAT=OLD.KDOBAT;
+SET @KELUAR=OLD.KDTRANS;
+SET @X=(SELECT COUNT(id) FROM tfifo WHERE KELUAR=@KELUAR AND KDOBAT=@KDOBAT ORDER BY ID  LIMIT 1);
+IF (@X > 0 ) THEN
+	REPEAT
+    	SET @ID=(SELECT ID FROM tfifo WHERE KELUAR=@KELUAR AND KDOBAT=@KDOBAT ORDER BY ID  LIMIT 1);
+    	SET @QTY=(SELECT QTY FROM tfifo WHERE ID=@ID);
+        SET @KDTRANS=(SELECT MASUK FROM TFIFO WHERE ID = @ID);
+		UPDATE TOBATIN SET SISA=SISA+@QTY, KELUAR=KELUAR-@QTY WHERE KDTRANS=@KDTRANS AND KDOBAT=@KDOBAT;
+        DELETE FROM TFIFO WHERE ID = @ID;
+    SET @X = @X - 1;
+	UNTIL@X = 0
+	END REPEAT;
+END IF;
+END
+$$
+DELIMITER ;
+DROP TRIGGER IF EXISTS `OBATOUT_AFINSERT`;
 DELIMITER $$
 CREATE TRIGGER `OBATOUT_AFINSERT` AFTER INSERT ON `tobatout` FOR EACH ROW BEGIN
-SET @KODE=NEW.KDOBAT;
+SET @KELUAR=NEW.KDTRANS;
+SET @KDOBAT=NEW.KDOBAT;
 SET @JKELUAR=NEW.QTY;
-SET @ID=(SELECT ID FROM TOBATIN WHERE KDOBAT=@KODE AND SISA>0 ORDER BY TGLTRANS  LIMIT 1);
+SET @ID=(SELECT id FROM tobatin WHERE kdobat=@KDOBAT AND sisa > 0 ORDER BY kdtrans  LIMIT 1);
 SET @SISA=(SELECT SISA FROM TOBATIN WHERE ID=@ID);
 REPEAT
 IF(@SISA>@JKELUAR) THEN
 UPDATE TOBATIN SET SISA=@SISA-@JKELUAR, KELUAR=KELUAR+@JKELUAR WHERE ID=@ID;
+SET @MASUK=(SELECT kdtrans FROM tobatin WHERE id=@ID); 
+INSERT INTO tfifo (MASUK, KELUAR, KDOBAT, QTY) VALUES (@MASUK, @KELUAR, @KDOBAT, @JKELUAR);
 SET @JKELUAR=0;
 END IF;
 IF (@SISA<@JKELUAR) THEN
 UPDATE TOBATIN SET SISA=0, KELUAR=KELUAR+@SISA WHERE ID=@ID;
+SET @MASUK=(SELECT kdtrans FROM tobatin WHERE id=@ID); 
+INSERT INTO tfifo (MASUK, KELUAR, KDOBAT, QTY) VALUES (@MASUK, @KELUAR, @KDOBAT, @SISA);
 SET @JKELUAR=@JKELUAR-@SISA;
-SET @ID=(SELECT ID FROM TOBATIN WHERE KDOBAT=@KODE AND SISA>0 ORDER BY TGLTRANS LIMIT 1);
+SET @ID=(SELECT ID FROM TOBATIN WHERE KDOBAT=@KDOBAT AND SISA>0 ORDER BY KDTRANS LIMIT 1);
 SET @SISA=(SELECT SISA FROM TOBATIN WHERE ID=@ID);
 END IF;
 IF(@SISA=@JKELUAR) THEN
 UPDATE TOBATIN SET SISA=0,KELUAR=KELUAR+@JKELUAR WHERE ID=@ID;
+SET @MASUK=(SELECT kdtrans FROM tobatin WHERE id=@ID); 
+INSERT INTO tfifo (MASUK, KELUAR, KDOBAT, QTY) VALUES (@MASUK, @KELUAR, @KDOBAT, @JKELUAR);
 SET @JKELUAR=0;
 END IF;
 UNTIL @JKELUAR=0
 END REPEAT;
+END
+$$
+DELIMITER ;
+DROP TRIGGER IF EXISTS `OBATOUT_AFUPDATE`;
+DELIMITER $$
+CREATE TRIGGER `OBATOUT_AFUPDATE` AFTER UPDATE ON `tobatout` FOR EACH ROW BEGIN
+SET @NQTY = NEW.QTY;
+SET @KDOBAT = NEW.KDOBAT;
+SET @KELUAR = NEW.KDTRANS;
+SET @OQTY = OLD.QTY;
+
+IF (@NQTY < @OQTY) THEN
+	SET @QTY = @OQTY - @NQTY;
+	SET @ID=(SELECT ID FROM TOBATIN WHERE KDOBAT=@KDOBAT AND KELUAR > 0 ORDER BY KDTRANS  DESC LIMIT 1);
+	SET @KELUAR=(SELECT KELUAR FROM TOBATIN WHERE ID=@ID);
+	REPEAT
+		IF(@KELUAR > @QTY) THEN
+			UPDATE TOBATIN SET SISA=SISA+@QTY, KELUAR=KELUAR-@QTY WHERE ID=@ID;
+			SET @QTY=0;
+		END IF;
+		IF (@KELUAR < @QTY) THEN
+			UPDATE TOBATIN SET KELUAR=0, SISA=SISA+@KELUAR WHERE ID=@ID;
+			SET @QTY=@QTY-@KELUAR;
+			SET @ID=(SELECT ID FROM TOBATIN WHERE KDOBAT=@KDOBAT AND KELUAR > 0 ORDER BY KDTRANS  DESC LIMIT 1);
+			SET @KELUAR=(SELECT KELUAR FROM TOBATIN WHERE ID=@ID);
+		END IF;
+		IF(@KELUAR = @QTY) THEN
+			UPDATE TOBATIN SET KELUAR=0, SISA=SISA+@KELUAR WHERE ID=@ID;
+			SET @QTY=0;
+		END IF;
+	UNTIL @QTY=0
+	END REPEAT;
+END IF;
+
+IF (@NQTY > @OQTY) THEN
+	SET @JKELUAR=@NQTY-@OQTY;
+	SET @ID=(SELECT ID FROM TOBATIN WHERE KDOBAT=@KDOBAT AND SISA>0 ORDER BY KDTRANS  LIMIT 1);
+	SET @SISA=(SELECT SISA FROM TOBATIN WHERE ID=@ID);
+	REPEAT
+		IF(@SISA>@JKELUAR) THEN
+			UPDATE TOBATIN SET SISA=@SISA-@JKELUAR, KELUAR=KELUAR+@JKELUAR WHERE ID=@ID;
+            SET @MASUK=(SELECT kdtrans FROM tobatin WHERE id=@ID);
+            INSERT INTO tfifo (MASUK, KELUAR, KDOBAT, QTY) VALUES (@MASUK, @KELUAR, @KDOBAT, @JKELUAR) ON DUPLICATE KEY UPDATE QTY=QTY+@JKELUAR;
+			SET @JKELUAR=0;
+		END IF;
+		IF (@SISA<@JKELUAR) THEN
+			UPDATE TOBATIN SET SISA=0, KELUAR=KELUAR+@SISA WHERE ID=@ID;
+			SET @MASUK=(SELECT kdtrans FROM tobatin WHERE id=@ID); 
+			INSERT INTO tfifo (MASUK, KELUAR, KDOBAT, QTY) VALUES (@MASUK, @KELUAR, @KDOBAT, @SISA) ON DUPLICATE KEY UPDATE QTY=QTY+@JKELUAR;
+			SET @JKELUAR=@JKELUAR-@SISA;
+			SET @ID=(SELECT ID FROM TOBATIN WHERE KDOBAT=@KDOBAT AND SISA>0 ORDER BY KDTRANS LIMIT 1);
+			SET @SISA=(SELECT SISA FROM TOBATIN WHERE ID=@ID);
+		END IF;
+		IF(@SISA=@JKELUAR) THEN
+			UPDATE TOBATIN SET SISA=0,KELUAR=KELUAR+@JKELUAR WHERE ID=@ID;
+			SET @MASUK=(SELECT kdtrans FROM tobatin WHERE id=@ID); 
+			INSERT INTO tfifo (MASUK, KELUAR, KDOBAT, QTY) VALUES (@MASUK, @KELUAR, @KDOBAT, @JKELUAR) ON DUPLICATE KEY UPDATE QTY=QTY+@JKELUAR;
+			SET @JKELUAR=0;
+		END IF;
+	UNTIL @JKELUAR=0
+	END REPEAT;
+END IF;
 END
 $$
 DELIMITER ;
@@ -132,6 +233,7 @@ DELIMITER ;
 -- Struktur dari tabel `tsatelit`
 --
 
+DROP TABLE IF EXISTS `tsatelit`;
 CREATE TABLE `tsatelit` (
   `id` int(11) NOT NULL,
   `kdsat` varchar(5) NOT NULL,
@@ -143,9 +245,9 @@ CREATE TABLE `tsatelit` (
 --
 
 INSERT INTO `tsatelit` (`id`, `kdsat`, `nmsat`) VALUES
-(2, '0001', 'Bangsal Parang Seling'),
-(3, '0002', 'Bangsal Parang Kusumo'),
-(4, '0003', 'Bangsal Ceplok Sriwedari');
+(1, '0001', 'Bangsal Parang Seling'),
+(2, '0002', 'Bangsal Parang Kusumo'),
+(3, '0003', 'Bangsal Ceplok Sriwedari');
 
 -- --------------------------------------------------------
 
@@ -153,6 +255,7 @@ INSERT INTO `tsatelit` (`id`, `kdsat`, `nmsat`) VALUES
 -- Struktur dari tabel `tsupp`
 --
 
+DROP TABLE IF EXISTS `tsupp`;
 CREATE TABLE `tsupp` (
   `id` int(11) NOT NULL,
   `kdsupp` varchar(15) NOT NULL,
@@ -165,17 +268,17 @@ CREATE TABLE `tsupp` (
 --
 
 INSERT INTO `tsupp` (`id`, `kdsupp`, `nmsupp`, `almsupp`) VALUES
-(3, '020230', 'PT. Megah Medika Pratama', ''),
-(4, '020302', 'PT. Surya Global', ''),
-(5, '020231', 'PT. Merapi Utama Pharma', ''),
-(6, '020001', 'Antarmitra Sembada, PT.', ''),
-(7, '020002', 'Anugrah Argon Medika, PT.', ''),
-(8, '020003', 'Dos Ni Roha, PT', ''),
-(9, '020004', 'Anugerah Pharmindo Lestari, PT', ''),
-(10, '020005', 'Bina Sanprima, PT', ''),
-(11, '020006', 'Enseval, PT', ''),
-(12, '020007', 'Parit Padang Global, PT', ''),
-(13, '020008', 'Millenium Pharmacon Int. PT', '');
+(1, '020230', 'PT. Megah Medika Pratama', ''),
+(2, '020302', 'PT. Surya Global', ''),
+(3, '020231', 'PT. Merapi Utama Pharma', ''),
+(4, '020001', 'Antarmitra Sembada, PT.', ''),
+(5, '020002', 'Anugrah Argon Medika, PT.', ''),
+(6, '020003', 'Dos Ni Roha, PT', ''),
+(7, '020004', 'Anugerah Pharmindo Lestari, PT', ''),
+(8, '020005', 'Bina Sanprima, PT', ''),
+(9, '020006', 'Enseval, PT', ''),
+(10, '020007', 'Parit Padang Global, PT', ''),
+(11, '020008', 'Millenium Pharmacon Int. PT', '');
 
 -- --------------------------------------------------------
 
@@ -183,6 +286,7 @@ INSERT INTO `tsupp` (`id`, `kdsupp`, `nmsupp`, `almsupp`) VALUES
 -- Struktur dari tabel `tuser`
 --
 
+DROP TABLE IF EXISTS `tuser`;
 CREATE TABLE `tuser` (
   `id` int(11) NOT NULL,
   `userid` varchar(15) NOT NULL,
@@ -201,6 +305,13 @@ INSERT INTO `tuser` (`id`, `userid`, `nama`, `email`, `passwd`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tfifo`
+--
+ALTER TABLE `tfifo`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `keluar` (`keluar`,`masuk`,`kdobat`) USING BTREE;
 
 --
 -- Indexes for table `tobat`
@@ -249,6 +360,11 @@ ALTER TABLE `tuser`
 --
 
 --
+-- AUTO_INCREMENT for table `tfifo`
+--
+ALTER TABLE `tfifo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
 -- AUTO_INCREMENT for table `tobat`
 --
 ALTER TABLE `tobat`
@@ -262,7 +378,7 @@ ALTER TABLE `tobatin`
 -- AUTO_INCREMENT for table `tobatout`
 --
 ALTER TABLE `tobatout`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT for table `tsatelit`
 --
